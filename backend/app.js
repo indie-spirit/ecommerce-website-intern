1194ba365c1af8534b6073aca1471cf068035bce
Ananth <krishnaathi27@gmail.com>
2022-08-09 12:38:27 +0530

    [Team GoF] Refactor frontend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,27 @@
-const mongoose = require('mongoose');
-const app= require('express');
-const dbURI = 'mongodb+srv://<------>@cluster0.ca2lshi.mongodb.net/?retryWrites=true&w=majority';
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
 
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(function(result) {
-        console.log('Database is connected');
-    })
-    .catch((err) => console.log(err));
+dotenv.config();
+
+let app = express();
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  console.log("Database Connected");
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.post('/', function (req, res) {
+   res.send('Hello World');
+})
+
+let server = app.listen(8081, function () {
+   let host = server.address().address
+   let port = server.address().port
+   
+   console.log("Example app listening at 8080")
+})
