83a09c15d72f9ad8738ed7eeb539e5299ef4e6ba
Jagath <jagathnanu@gmail.com>
2022-10-17 23:17:56 +0530

    Password will be Hashed While Storing in DB

--- backend/src/models/admin.js
+++ backend/src/models/admin.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const AdminSchema = new mongoose.Schema({
     name: {
@@ -23,6 +24,17 @@ const AdminSchema = new mongoose.Schema({
     }
 });
 
+AdminSchema.pre('save', async function(next){
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password=hashedPassword
+        next()
+    }
+    catch(error){
+     next(error);
+    }
+})
 const Admin = new mongoose.model("Admin", AdminSchema);
 
 export default Admin;
