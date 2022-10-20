d3e34b810e47ac7f83639667f750c75203fbe326
Jagath <jagathnanu@gmail.com>
2022-10-20 11:00:47 +0530

    Product update and Delete Model Updated

--- backend/src/controller/product.js
+++ backend/src/controller/product.js
@@ -18,15 +18,41 @@ export const createProduct = async (req, res) => {
     })
     try {
         const dbUser = await Product.findOne({name});
-        
         if (dbUser) {
             return res.status(403).json({ message: "Product Already Exists"});
         }
         await product.save();
-    } catch (error) {
+    } 
+    catch (error) {
         console.log(error.message);
         return res.status(500).json({ message: "Internal Server Error" });
     }
 
     return res.status(200).send({ message: "Product Created" });
 };
+export const updateProduct = async (req,res) => {
+    const {name,price,description,type,brand} = req.body;
+    const filter = {name};
+    const dbUser = await Product.findOne({name});
+    if(!dbUser){
+        return res.status(404).send({"message":"Product Not Found"});
+    }
+
+    const updateProduct = await Product.updateOne(filter, req.body);
+    if(updateProduct){
+            return res.status(200).send({"message":"Successfully Updated"});
+    }
+    
+
+}
+
+export const deleteProduct = async (req,res) => {
+    const{
+        name
+    } = req.body;
+    const product = await Product.findOneAndDelete({name});
+    if(product){
+        return res.status(200).send({"message":"Product Deleted"});
+    }
+    return res.status(404).send({"message":"Product not Found"});
+}
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createProduct } from "../controller/product.js";
+import { createProduct, deleteProduct, updateProduct } from "../controller/product.js";
 
 const router = Router();
 
 router.post("/", createProduct);
-
+router.post("/update", updateProduct);
+router.delete("/delete", deleteProduct);
 export default router;
