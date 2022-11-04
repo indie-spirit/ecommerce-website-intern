b6d4bc9b90a81a325e503efccac551b09577a00b
jagajeet <jagajeetkuppala99@gmail.com>
2022-11-04 17:46:56 +0530

    [Beagle] Fix Public folder and remove unwanted files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import Abi from './Components/abi';
 
 function App() {
   return (
     <div className="App">
-      <p>app file </p>
-      <Abi desc="welcome" name=" abi"/>
-      <Abi desc="heloo"/>
-      {/* <Button title="home page" to="https://www.google.com/"/>
-      <Button title="about page" to="https://www.w3schools.com/tags/tag_table.asp"/>
-      <table>
-        <TableHeader/>
-      </table> */}
+      <p>E Commerce Website</p>
     </div>
   );
 }
